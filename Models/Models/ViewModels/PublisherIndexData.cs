988cb6896c878480bab1876978652aa3f00fd0c6
alexto08 <147998065+alexto08@users.noreply.github.com>
2023-11-19 23:34:17 +0200

    Add files via upload

---
+++ Models/Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,10 @@
+﻿using Toma_Alexandru_Lab2.Models;
+
+namespace Toma_Alexandru_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
