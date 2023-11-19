a007cd87ffa20aeff40ca0ac3215d2d4d2e05c1a
alexto08 <147998065+alexto08@users.noreply.github.com>
2023-11-19 23:25:40 +0200

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
+
+
+namespace Toma_Alexandru_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        [Display(Name = "Book Title")]
+        public string Title { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
+        [Range(0.01, 500)]
+
+
+        public int? AuthorID { get; set; }
+        public Author? Author { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
+
+        public decimal Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
+        
+        public int? PublisherID { get; set; }
+        public Publisher? Publisher { get; set; }
+        
+
+        public int? BorrowingID { get; set; }
+        public Borrowing? Borrowing { get; set; }
+        public ICollection<BookCategory>? BookCategories { get; set; }
+    }
+}
+
