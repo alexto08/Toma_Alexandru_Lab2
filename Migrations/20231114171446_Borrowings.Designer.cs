bdc23da58c7090c49d09509d66ebfe79cfee91f3
alexto08 <147998065+alexto08@users.noreply.github.com>
2023-11-19 23:28:15 +0200

    Add files via upload

--- Migrations/20231114171446_Borrowings.Designer.cs
+++ Migrations/20231114171446_Borrowings.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Toma_Alexandru_Lab2.Data;
 
@@ -11,9 +12,10 @@ using Toma_Alexandru_Lab2.Data;
 namespace Toma_Alexandru_Lab2.Migrations
 {
     [DbContext(typeof(Toma_Alexandru_Lab2Context))]
-    partial class Toma_Alexandru_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20231114171446_Borrowings")]
+    partial class Borrowings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
